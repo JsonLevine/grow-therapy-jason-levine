810549b1f1067bf5f52c5c54ebe3d78ccf136408
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2024-01-25 20:58:00 -0500

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,27 +5,57 @@ export default function Pagination({page, articlesPerPage, totalArticles, pull_p
 
   // Find the new page number after pagination to propegate upwards (add 1 to account for page 0)
   const paginate = ({ selected }) => {
-    pull_page(selected + 1);
+    pull_page(selected);
+  };
+
+  const pages = Math.ceil(totalArticles / articlesPerPage);
+  function handleNext() {
+    pull_page(page+1);
+  };
+  
+  // handler for the previous button
+  function handlePrevious() {
+    pull_page(page-1);
   };
 
   if (totalArticles) {
     return (
-      <ReactPaginate
-        onPageChange={paginate}
-        pageCount={Math.ceil(totalArticles / articlesPerPage)}
-        previousLabel={'<'}
-        nextLabel={'>'}
-        pageRangeDisplayed={2}
-        containerClassName={'pagination desktop-8'}
-        pageLinkClassName={'page-number'}
-        previousLinkClassName={'page-number'}
-        nextLinkClassName={'page-number'}
-        activeLinkClassName={'active'}
-        forcePage={page-1}
-        siblingCount='1'
-      />
+    <div className="pagination">
+      <Button onClick={handlePrevious} disabled={page <= 1}>{'<'}</Button>
+      { pages - page < 1 && page - 4 > 0 && <Button onClick={() => pull_page(page - 4)}>{page - 4}</Button> }
+      { pages - page < 2 && page - 3 > 0 && <Button onClick={() => pull_page(page - 3)}>{page - 3}</Button> }
+      { pages && page - 2 > 0 && <Button onClick={() => pull_page(page - 2)}>{page - 2}</Button> }
+      { pages && page - 1 > 0 && <Button onClick={() => pull_page(page - 1)}>{page - 1}</Button> }
+      <Button active={true}>{page}</Button>
+      { page + 1 <= pages && <Button onClick={() => pull_page(page + 1)}>{page + 1}</Button> }
+      { page + 2 <= pages && <Button onClick={() => pull_page(page + 2)}>{page + 2}</Button> }
+      { page + 3 <= pages && page < 3 && <Button onClick={() => pull_page(page + 3)}>{page + 3}</Button> }
+      { page + 4 <= pages && page < 2 && <Button onClick={() => pull_page(page + 4)}>{page + 4}</Button> }
+      <Button onClick={handleNext} disabled={page === pages}>{'>'}</Button>
+    </div>
     )
   } else {
     return null
   }
 }
+
+function Button({ children, ...props }) {
+  return (
+    <button className={props.active ? 'active page-number' : 'page-number' } {...props}>{children}</button>
+  );
+};
+
+{/* <ReactPaginate
+onPageChange={paginate}
+pageCount={Math.ceil(totalArticles / articlesPerPage)}
+previousLabel={'<'}
+nextLabel={'>'}
+pageRangeDisplayed={2}
+containerClassName={'pagination desktop-8'}
+pageLinkClassName={'page-number'}
+previousLinkClassName={'page-number'}
+nextLinkClassName={'page-number'}
+activeLinkClassName={'active'}
+forcePage={page-1}
+siblingCount='1'
+/> */}
